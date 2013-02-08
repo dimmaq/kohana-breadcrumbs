6131a9cd6dfa39ae20ad0c3e75757f4026e38584
Dmitrij K <dimmaq@ya.ru>
2013-02-09 01:28:27 +0400

    update view

--- views/blocks/breadcrumbs.php
+++ views/blocks/breadcrumbs.php
@@ -1,30 +1,3 @@
 <?php
-	if (count($breadcrumbs) > 1)
-	{
-		$f = TRUE;
-		foreach ($breadcrumbs as $item)
-		{
-			if ($f)
-				$f = FALSE;
-			else
-				echo '&nbsp;/&nbsp;';
-			$l = $item['link'];
-			$t = $item['text'];
-			if (43 < UTF8::strlen($t))
-			{
-				$t = UTF8::substr($t, 0, 40) . '...';
-			}
-			if ($l)
-				echo '<a href="'.$l.'">'.HTML::entities($t).'</a>';
-			else
-				echo HTML::entities($t);
-		}
-	}
-?>
-<!--
-<pre>
-<?php
-	print_r($breadcrumbs);
-?>
-</pre>
--->
+	foreach ($breadcrumbs as $item)
+		echo HTML::anchor($item['link'], $item['text']), '<span>/</span>';
